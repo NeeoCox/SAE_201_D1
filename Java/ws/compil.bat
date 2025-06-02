080cc200ef023e1560f76ae05303389da7e26714
NeeoCox <maelcoignard16@gmail.com>
2025-06-02 11:51:00 +0200

    new file:   Java/class/controler/Controler.class 	modified:   Java/class/vue/App.class 	modified:   Java/src/controler/Controler.java 	modified:   Java/src/vue/App.java 	modified:   Java/ws/compil.bat

--- Java/ws/compil.bat
+++ Java/ws/compil.bat
@@ -1,5 +1,8 @@
-javac -sourcepath ../src -d ../class --module-path C:\Users\maelc\Desktop\Is_All\BUTnfo\Semestre_2\SAE\SAE2_01\SAE_201_D1\java\javafx-sdk-21.0.7\lib --add-modules javafx.controls,javafx.graphics,javafx.fxml ../src/vue/App.java
+@echo off
+set FX="C:\Users\maelc\Desktop\Is_All\BUTnfo\Semestre_2\SAE\SAE2_01\SAE_201_D1\java\javafx-sdk-21.0.7\lib"
 
-java -cp ../class --module-path C:\Users\maelc\Desktop\Is_All\BUTnfo\Semestre_2\SAE\SAE2_01\SAE_201_D1\java\javafx-sdk-21.0.7\lib --add-modules javafx.controls,javafx.graphics,javafx.fxml vue.App
+javac --module-path %FX% --add-modules javafx.controls,javafx.graphics,javafx.fxml -sourcepath ../src -d ../class ../src/vue/App.java
+
+java --module-path %FX% --add-modules javafx.controls,javafx.graphics,javafx.fxml -cp ../class vue.App
 
 pause
