595b3e2f9d8c7658bc6854ee73c1b55d946c6f9d
NeeoCox <maelcoignard16@gmail.com>
2025-06-02 11:42:28 +0200

    new file:   Java/class/vue/App.class 	modified:   Java/fxml/pageFxml/PageConnexion/PageConnexion.fxml 	modified:   Java/src/controler/Controler.java 	modified:   Java/ws/compil.bat
    	modified:   Java/src/controler/Controler.java

--- Java/src/controler/Controler.java
+++ Java/src/controler/Controler.java
@@ -1,10 +1,56 @@
 package controler;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.stage.Stage;
 /**
  * Controller
  */
 public class Controler {
 
+	private Stage stage;
+
+
+
+	public Controler(){
+		System.out.println("controler");
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+	
+	public void goToPageAdminAcceuil(){
+		System.out.println("goToPageAdminAcceuil");
+		this.goTo("file:../fxml/pageFxml/Administateur/PageAdminAcceuil.fxml");
+		stage.setResizable(true);
+	}
+
+	private void goTo(String fichier){
+		try {
+			FXMLLoader loader = new FXMLLoader(new URL(url));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			Controller.stage.setScene(scene);
+			Controller.stage.centerOnScreen();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 	/**
 	public void versPageLogin(ActionEvent e){
 		
