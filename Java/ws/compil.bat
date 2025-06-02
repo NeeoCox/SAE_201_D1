e611a06a0d5987b5b84c3f18c1678eaef4623430
NeeoCox <maelcoignard16@gmail.com>
2025-06-02 11:42:28 +0200

    new file:   Java/class/vue/App.class 	modified:   Java/fxml/pageFxml/PageConnexion/PageConnexion.fxml 	modified:   Java/src/controler/Controler.java 	modified:   Java/ws/compil.bat
    	modified:   Java/src/controler/Controler.java

--- Java/ws/compil.bat
+++ Java/ws/compil.bat
@@ -1,3 +1,5 @@
-javac -d ..\class ..\src\metier\persistence\*.java
+javac -sourcepath ../src -d ../class --module-path C:\Users\maelc\Desktop\Is_All\BUTnfo\Semestre_2\SAE\SAE2_01\SAE_201_D1\java\javafx-sdk-21.0.7\lib --add-modules javafx.controls,javafx.graphics,javafx.fxml ../src/vue/App.java
+
+java -cp ../class --module-path C:\Users\maelc\Desktop\Is_All\BUTnfo\Semestre_2\SAE\SAE2_01\SAE_201_D1\java\javafx-sdk-21.0.7\lib --add-modules javafx.controls,javafx.graphics,javafx.fxml vue.App
 
 pause
