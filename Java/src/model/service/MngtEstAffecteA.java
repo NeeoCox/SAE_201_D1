89c7cac30c207876348d5e43f1df05a42d1a52d2
AdrienCdr29 <adriencoudiere974@gmail.com>
2025-06-05 14:59:08 +0200

    new file:   Java/src/dao/DAOEstDisponible.java 	modified:   Java/src/model/persistence/EstDisponible.java 	deleted:    Java/src/model/service/AffectationMngt.java 	deleted:    Java/src/model/service/DPSMngt.java 	deleted:    Java/src/model/service/DispoMngt.java 	new file:   Java/src/model/service/MngtDPS.java 	new file:   Java/src/model/service/MngtEstAffecteA.java 	new file:   Java/src/model/service/MngtEstDisponible.java 	new file:   Java/src/model/service/MngtSecouriste.java 	deleted:    Java/src/model/service/SecouristeMngt.java

---
+++ Java/src/model/service/MngtEstAffecteA.java
@@ -0,0 +1,60 @@
+package model.service;
+
+import dao.DAOEstAffecteA;
+import java.sql.SQLException;
+import java.util.List;
+import model.persistence.*;
+
+/**
+ * Classe de gestion des affectations (EstAffecteA) avec persistance en base.
+ * Utilise DAOEstAffecteA pour toutes les opérations.
+ */
+public class MngtEstAffecteA {
+
+    private final DAOEstAffecteA daoAffectation;
+
+    public MngtEstAffecteA(DAOEstAffecteA daoAffectation) {
+        this.daoAffectation = daoAffectation;
+    }
+
+    /**
+     * Créer une affectation et l'ajouter en base.
+     */
+    public void creerAffectation(long idSecouriste, String intituleCompetence, long idDPS) throws SQLException {
+        EstAffecteA affectation = new EstAffecteA();
+        affectation.setIdSecouriste((int)idSecouriste);
+        affectation.setIntituleCompetence(intituleCompetence);
+        affectation.setIdDPS(idDPS);
+        daoAffectation.create(affectation);
+    }
+
+    /**
+     * Modifier une affectation existante en base.
+     */
+    public void modifierAffectation(long idSecouriste, String intituleCompetence, long idDPS) throws SQLException {
+        EstAffecteA affectation = new EstAffecteA();
+        affectation.setIdSecouriste((int)idSecouriste);
+        affectation.setIntituleCompetence(intituleCompetence);
+        affectation.setIdDPS(idDPS);
+        daoAffectation.update(affectation);
+    }
+
+    /**
+     * Supprimer une affectation de la base.
+     */
+    public void supprimerAffectation(long idSecouriste, long idDPS) throws SQLException {
+        daoAffectation.delete((int)idSecouriste, idDPS);
+    }
+
+    /**
+     * Lister toutes les affectations (affiche les détails).
+     */
+    public void listerAffectations() throws SQLException {
+        List<EstAffecteA> listeAffectations = daoAffectation.readAll();
+        for (EstAffecteA a : listeAffectations) {
+            System.out.println("Affectation: Secouriste ID: " + a.getIdSecouriste() +
+                               ", Compétence: " + a.getIntituleCompetence() +
+                               ", DPS ID: " + a.getIdDPS());
+        }
+    }
+}
