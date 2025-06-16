f4e79330d7f7c425d825a95ed3d239ca6863f2ae
Léa <vimart.e2402188@etud.univ-ubs.fr>
2025-06-16 15:46:55 +0200

    Classes DAO ET Test pour se co à la bdd

---
+++ Java/src/model/dao/DAO.java
@@ -0,0 +1,37 @@
+package model.dao;
+
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DAO<T> {
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url="jdbc:mysql://localhost:3306/Secouristes"; //changer pour avoir acces au donnée des commune 
+    private static String username = "client";
+    private static String pwd = "password";
+    Connection connection;
+
+
+    static Connection createConnection() throws SQLException{
+        try{
+            Class.forName(driver);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return DriverManager.getConnection(url, username, pwd);
+    }
+
+    public static void setUsername(String username) {
+        if(username == null) throw new IllegalArgumentException("username null");
+        if(username.length() == 0) throw new IllegalArgumentException("username vide");
+        DAO.username = username;
+    }
+    
+    public static void setPwd(String pwd) {
+        if(username == null) throw new IllegalArgumentException("mot de passe null");
+        if(username.length() == 0) throw new IllegalArgumentException("mot de passe vide");
+        DAO.pwd = pwd;
+    }
+}
