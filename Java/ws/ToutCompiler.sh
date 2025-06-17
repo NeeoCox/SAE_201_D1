a4f7136746ecba1d6d81de3bbd5c7198e53adddc
Léa <vimart.e2402188@etud.univ-ubs.fr>
2025-06-17 14:15:57 +0200

    new file:   Java/ws/ToutCompiler.sh 	new file:   Java/ws/compileDAO.sh

---
+++ Java/ws/ToutCompiler.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Compilation des fichiers Java
+
+javac --module-path ../lib/javafx --add-modules javafx.controls,javafx.graphics,javafx.fxml \
+  -sourcepath ../src -d ../class ../src/controller/*.java
+
+javac -d ../class -sourcepath ../src ../src/model/dao/*.java
+javac -d ../class -sourcepath ../src ../src/model/graph/*.java
+
+# Les deux lignes suivantes sont commentées comme dans le fichier original
+# javac -d ../class -sourcepath ../src ../src/graph/algorithm/*.java
+# javac -d ../class -sourcepath ../src ../src/graph/graphmodel/*.java
+
+javac -d ../class -sourcepath ../src ../src/model/persistence/*.java
+javac -d ../class -sourcepath ../src ../src/model/service/*.java
+
+javac --module-path ../lib/javafx --add-modules javafx.controls,javafx.graphics,javafx.fxml \
+  -sourcepath ../src -d ../class ../src/view/App.java
+
+# Exécution de l'application
+java --module-path ../lib/javafx --add-modules javafx.controls,javafx.graphics,javafx.fxml \
+  -cp ../fxml:../class view.App
---
+++ Java/ws/compileDAO.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+javac -cp ".:../lib/mysql/mysql-connector-j-9.3.0.jar:../src" -d ../class ../src/model/dao/*.java
+
+java -cp "../class:../lib/mysql/mysql-connector-j-9.3.0.jar" model.dao.TestConnexion
