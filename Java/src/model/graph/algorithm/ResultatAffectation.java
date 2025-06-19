d0e5fff83092b7ccdde28effb212ab1947e30d72
AdrienCdr29 <adriencoudiere974@gmail.com>
2025-06-19 20:27:05 +0200

    modified:   Java/class/controller/Controller.class 	modified:   Java/class/model/graph/algorithm/ResultatAffectation.class 	modified:   Java/class/model/graph/algorithm/exhaustive/AffectationExhaustive.class 	modified:   Java/class/model/graph/algorithm/greedy/AffectationGloutonne.class 	modified:   Java/src/controller/Controller.java 	modified:   Java/src/model/graph/algorithm/ResultatAffectation.java 	modified:   Java/src/model/graph/algorithm/exhaustive/AffectationExhaustive.java 	modified:   Java/src/model/graph/algorithm/greedy/AffectationGloutonne.java

--- Java/src/model/graph/algorithm/ResultatAffectation.java
+++ Java/src/model/graph/algorithm/ResultatAffectation.java
@@ -2,18 +2,23 @@ package model.graph.algorithm;
 
 import java.util.List;
 import java.util.Map;
-
 import model.persistence.DPS;
 import model.persistence.Secouriste;
 
 public class ResultatAffectation {
     private final Map<DPS, List<Secouriste>> affectations;
+    private final Map<BesoinUnitaire, Secouriste> affectationsUnitaires;
 
-    public ResultatAffectation(Map<DPS, List<Secouriste>> affectations) {
+    public ResultatAffectation(Map<DPS, List<Secouriste>> affectations, Map<BesoinUnitaire, Secouriste> affectationsUnitaires) {
         this.affectations = affectations;
+        this.affectationsUnitaires = affectationsUnitaires;
     }
 
     public Map<DPS, List<Secouriste>> getAffectations() {
         return affectations;
+    }
+
+    public Map<BesoinUnitaire, Secouriste> getAffectationsUnitaires() {
+        return affectationsUnitaires;
     }
 }
